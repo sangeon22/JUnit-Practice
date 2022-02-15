23469fc8089cafe93b116384feba8fc7e3064207
sangeon22 <tkddjsdl33@naver.com>
2022-02-15 23:11:21 +0900

    Spring Framework JUnit Test Practice

---
+++ src/test/java/com/example/springcalculator/SpringCalculatorApplicationTests.java
@@ -0,0 +1,28 @@
+package com.example.springcalculator;
+
+import com.example.springcalculator.component.Calculator;
+import com.example.springcalculator.component.DollarCalculator;
+import com.example.springcalculator.component.MarketApi;
+import com.example.springcalculator.controller.CalculatorApiController;
+import com.example.springcalculator.dto.Req;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@SpringBootTest
+class SpringCalculatorApplicationTests {
+
+
+
+}
